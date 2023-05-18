c0256d2cda58327516b389de4a2248444b03889a
S <s@MacBook-Pro-16.local>
2023-05-18 11:52:55 +0900

    gatherfind_lamda

--- src/string_number/ex04/GatherFind.java
+++ src/string_number/ex04/GatherFind.java
@@ -6,21 +6,35 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class GatherFind {
+//  private static final Set<Character> allVowels = new HashSet(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+//
+//  public static Pair<Integer, Integer> countVowelsAndConsonants(String str) {
+//    str = str.toLowerCase();
+//    int vowels = 0;
+//    int consonants = 0;
+//
+//    for(int i=0; i<str.length(); i++) {
+//      char ch = str.charAt(i);
+//      if(allVowels.contains(ch)) {
+//        vowels++;
+//      } else if((ch >= 'a' && ch <= 'z')) {
+//        consonants++;
+//      }
+//    }
+//    return Pair.of(vowels, consonants);
+//  }
   private static final Set<Character> allVowels = new HashSet(Arrays.asList('a', 'e', 'i', 'o', 'u'));
 
-  public static Pair<Integer, Integer> countVowelsAndConsonants(String str) {
+  public static Pair<Long, Long> countVowelsAndConsonants(String str) {
     str = str.toLowerCase();
-    int vowels = 0;
-    int consonants = 0;
+    long vowels = str.chars()
+        .filter(ch -> allVowels.contains((char) ch))
+        .count();
 
-    for(int i=0; i<str.length(); i++) {
-      char ch = str.charAt(i);
-      if(allVowels.contains(ch)) {
-        vowels++;
-      } else if((ch >= 'a' && ch <= 'z')) {
-        consonants++;
-      }
-    }
+    long consonants = str.chars()
+        .filter(ch -> (ch >= 'a' && ch <= 'z'))
+        .count();
+
     return Pair.of(vowels, consonants);
   }
 }
